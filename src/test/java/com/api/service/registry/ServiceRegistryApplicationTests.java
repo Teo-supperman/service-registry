cbbe8873a742dbd364a9a2dd6940996b64b71041
tobi <tai.mongtri@gmail.com>
2023-04-09 01:44:35 +0700

    init commit

---
+++ src/test/java/com/api/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
